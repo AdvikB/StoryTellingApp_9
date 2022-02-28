9a25d61b4d591fe6d8f9139504bc9bdad673a8ca
AdvikB <73247534+AdvikB@users.noreply.github.com>
2022-02-27 20:40:52 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export
+const firebaseConfig = {
+  apiKey: "AIzaSyCIwcIW-FZOitcrn_jD7iG22KPyPQgor3w",
+  authDomain: "spectagram-11bf1.firebaseapp.com",
+  projectId: "spectagram-11bf1",
+  storageBucket: "spectagram-11bf1.appspot.com",
+  messagingSenderId: "841046584631",
+  appId: "1:841046584631:web:816ab17c3f58b55449beb2"
+};
